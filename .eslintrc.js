0aff4c11de59b7b285b95e7fb0a27f6495ed3a04
Naka <andresnakanishi@gmail.com>
2022-04-23 15:12:39 -0500

    [ADD] Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
+const RULES = {
+  OFF: "off",
+  WARN: "warn",
+  ERROR: "error",
+};
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "standard", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "standard"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: "latest",
+
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+const nakanishi = "nakanishi";
+
+console.log(`Yo soy el verdadero ${nakanishi}`);
+
+function alohar() {
+  console.log("Yo trabajo en Alohar");
+}
